98eb95000575c0baaa638c45ec66cc5c4431215d
Limpanfx <145815319+Limpanfx@users.noreply.github.com>
2024-09-09 14:06:43 +0200

    Code.py

--- ZeroTwo/Code.py
+++ ZeroTwo/Code.py
@@ -1,16 +1,25 @@
 import ctypes
 import os
+import time
 
 def change_wallpaper(path):
     abs_path = os.path.abspath(path)
     ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 3)
 
-imageSelector = int(input("What wallpaper do you want to use? (1,2,3,4) "))
-if imageSelector == 1:
-    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwoVirus\\WallPaper\\ZeroTwoCircle.jpg")
-elif imageSelector == 2:
-    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwoVirus\\WallPaper\\ZeroTwo1.png")
-elif imageSelector == 3:
-    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwoVirus\\WallPaper\\ZeroTwo2.jpg")
-elif imageSelector == 4:
-    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwoVirus\\WallPaper\\ZeroTwo3.jpeg")
+while True:
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwoCircle.jpg")
+    time.sleep(0.002)
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo1.png")
+    time.sleep(0.002)
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo2.jpg")
+    time.sleep(0.002)
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo3.jpeg")
+    time.sleep(0.002)
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo4.webp")
+    time.sleep(0.002)
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo5.jpg")
+    time.sleep(0.002)    
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo6.jpg")
+    time.sleep(0.002)    
+    change_wallpaper("C:\\Users\\23liot\\Desktop\\ZeroTwo\\WallPaper\\ZeroTwo7.jpg")
+    time.sleep(0.002)    
